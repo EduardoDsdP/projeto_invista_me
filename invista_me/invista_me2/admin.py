e37450e9a1b1d915f5ad2313d2d84f079623f069
EduardoDsdP <eduardodsdp@gmail.com>
2024-07-05 12:03:10 -0300

    first commit

---
+++ invista_me/invista_me2/admin.py
@@ -0,0 +1,9 @@
+from django.contrib import admin
+from .models import Investimento
+
+admin.site.register(Investimento)
+
+# Aqui registramos as funcionalidas que ficarão da url final admin. Aqui estamos registrando nosso model que é a class Invertimentos. Que por ser model, alem de uma classe é uma tabela de banco de dados. 
+
+
+
---
+++ invista_me/invista_me2/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class InvistaMe2Config(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'invista_me2'
